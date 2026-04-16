23b3a4375ef4ab8a192cd67a0ae98486c964e30c
Yeisson <yeissonalberto14@gmail.com>
2026-04-15 23:41:43 -0600

    Quiz de madera implementado

--- mi-primer-proyecto-react/src/Components/SaludoYeisson.jsx
+++ mi-primer-proyecto-react/src/Components/SaludoYeisson.jsx
@@ -1,15 +1,46 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './SaludoYeisson.css'
 
 export default function SaludoYeisson() {
+  //#1 decaramos todos 
+  const [phrase, setPhrase]= useState([]);
+  const [randomNumber, setRandomNumber]=useState(0);
   const [name] = useState('Yeisson')
 
+  // #2 Cargamos la prase motivacional al inicia el componente 
+  const motivacion = useEffect(() => {
+  const fetchPhrase = async () => {
+    try {
+      const response = await fetch("https://www.positive-api.online/phrases/esp"); 
+      const data = await response.json();
+
+      setPhrase(data);
+    } catch (error)
+    {
+      console.error("Error fetching phrase:", error);
+    }
+    finally
+    {
+      const randomIndex=Math.floor(Math.random() * 40);
+      setRandomNumber(randomIndex);
+    }
+  }
+
+  fetchPhrase(); 
+  },[]); 
+
+  // #3 renderizamos al componente 
   return (
-    <section className="saludo yeisson">
+    <div className="saludo yeisson">
       <h2 className="saludo_title">¡Hola, {name}!</h2>
       <p className="saludo_extra">
         ¡Aquí hay un mensaje adicional!
       </p>
-    </section>
+      <p>{phrase[randomNumber]?.text}</p>
+       <button onClick={()=> setRandomNumber(Math.floor(Math.random()*40))}>NUEVO MENSAJE</button>
+    </div>
   )
 }
+
+
+ 
